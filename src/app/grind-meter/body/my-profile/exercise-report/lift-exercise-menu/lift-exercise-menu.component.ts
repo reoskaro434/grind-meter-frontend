340cb3a67628ae3bd5e001ff5edfa98a3cb7d448
reoskaro434 <reoskaro434@gmail.com>
2023-11-11 12:17:32 +0100

    Fixed error with latest exercise that wwas now downloaded if list were greater than 1

--- src/app/grind-meter/body/my-profile/exercise-report/lift-exercise-menu/lift-exercise-menu.component.ts
+++ src/app/grind-meter/body/my-profile/exercise-report/lift-exercise-menu/lift-exercise-menu.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {Exercise} from "../../../../../models/exercise";
 import {LiftExerciseReport} from "../../../../../models/lift-exercise-report";
 import {WeightUnit} from "../../../../../models/weight";
@@ -13,8 +13,8 @@ import {ToastType} from "../../../../../enums/toast-type";
   templateUrl: './lift-exercise-menu.component.html',
   styleUrls: ['./lift-exercise-menu.component.css']
 })
-export class LiftExerciseMenuComponent implements OnInit{
-  @Input() exercise!: Exercise;
+export class LiftExerciseMenuComponent {
+  public _exercise!: Exercise;
 
   liftExerciseReport: LiftExerciseReport | undefined;
   inputSets: number = 0;
@@ -24,25 +24,33 @@ export class LiftExerciseMenuComponent implements OnInit{
   constructor(private exerciseReportApiCaller: ExerciseReportApiCallerService,
               private toast: ToastService) {
   }
-  ngOnInit() {
-   this.liftExerciseReport= {
-      exercise: this.exercise,
-      sets: [],
-      timestamp: new Date().setHours(0,0,0,0)
-    };
-   this.liftExerciseReport.exercise = this.exercise;
 
-   this.exerciseReportApiCaller.getLastReport(this.exercise.id).pipe(map((report) => {
+  @Input()
+  set exercise(exercise: Exercise) {
+    this.liftExerciseReport = undefined;
+    this.getLastExerciseReport(exercise.id);
+    this._exercise = exercise;
+  }
+
+  private getLastExerciseReport(exerciseId: string) {
+    this.exerciseReportApiCaller.getLastReport(exerciseId).pipe(map((report) => {
+        this.liftExerciseReport = {
+          exercise: this._exercise,
+          sets: [],
+          timestamp: new Date().setHours(0, 0, 0, 0)
+        };
         if (report != null) {
-          this.inputSets = report.sets.length;
           this.liftExerciseReport!.sets = report.sets;
+          this.inputSets = this.liftExerciseReport.sets.length;
+          this.updateExerciseArrays(this.inputSets);
+        }else {
+          this.inputSets = 0;
+          this.updateExerciseArrays(0);
         }
-     }),
-     catchError(err => {
-       throw err;
-     })).subscribe();
-
-   this.updateExerciseArrays(this.liftExerciseReport.sets.length);
+      }),
+      catchError(err => {
+        throw err;
+      })).subscribe();
   }
 
   onSubmit() {
@@ -56,10 +64,10 @@ export class LiftExerciseMenuComponent implements OnInit{
       .pipe(map((response) => {
           this.toast.showMessage("Report saved!", ToastType.SUCCESS);
         }),
-      catchError(err => {
-        this.toast.showMessage("Could not save the report!", ToastType.ERROR);
-        throw err;
-      })).subscribe();
+        catchError(err => {
+          this.toast.showMessage("Could not save the report!", ToastType.ERROR);
+          throw err;
+        })).subscribe();
   }
 
   updateExerciseArrays(inputSeries: number) {
@@ -68,11 +76,11 @@ export class LiftExerciseMenuComponent implements OnInit{
     }
     const currentLength = this.liftExerciseReport.sets.length;
 
-    if(currentLength > inputSeries) {
-      this.liftExerciseReport.sets.splice(-(currentLength-inputSeries));
+    if (currentLength > inputSeries) {
+      this.liftExerciseReport.sets.splice(-(currentLength - inputSeries));
     }
 
-    if(currentLength < inputSeries) {
+    if (currentLength < inputSeries) {
       const missingFields = inputSeries - currentLength;
       for (let i = 0; i < missingFields; i++) {
         this.liftExerciseReport.sets.push({
@@ -84,7 +92,7 @@ export class LiftExerciseMenuComponent implements OnInit{
     }
   }
 
-  isInputRepetitionsValid(value: number){
+  isInputRepetitionsValid(value: number) {
     if (value) {
       return this.repetitionsRegex.test(String(value));
     }
@@ -92,7 +100,7 @@ export class LiftExerciseMenuComponent implements OnInit{
     return false;
   }
 
-  isInputWeightValid(value: number){
+  isInputWeightValid(value: number) {
     if (value) {
       return this.weightRegex.test(String(value));
     }
