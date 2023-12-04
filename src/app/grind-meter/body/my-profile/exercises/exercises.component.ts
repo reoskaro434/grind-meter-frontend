f5cba4e79312ae1f3bdc03102740004ec80412c0
reoskaro434 <reoskaro434@gmail.com>
2023-12-04 22:57:49 +0100

    simplified exercises page, removed limits

--- src/app/grind-meter/body/my-profile/exercises/exercises.component.ts
+++ src/app/grind-meter/body/my-profile/exercises/exercises.component.ts
@@ -7,6 +7,8 @@ import {SelectionModel} from "@angular/cdk/collections";
 import {ToastType} from "../../../../enums/toast-type";
 import {catchError} from "rxjs/operators";
 import {ActivatedRoute, Router} from "@angular/router";
+import {Plan} from "../../../../models/plan";
+import {TreeNode} from "primeng/api";
 
 @Component({
   selector: 'app-exercises',
@@ -14,51 +16,34 @@ import {ActivatedRoute, Router} from "@angular/router";
   styleUrls: ['./exercises.component.css']
 })
 export class ExercisesComponent implements OnInit {
+  exercisesTree: TreeNode[] = [];
   exercises: Exercise[] = [];
-  displayedColumns: string[] = ['name', 'type', 'isActive', 'Actions'];
-  selection = new SelectionModel<boolean>(true, []);
-  readonly MAX_EXERCISES_LIST = 30;
   constructor(private exerciseApiCaller: ExerciseApiCallerService,
-              private toast: ToastService,
               private router: Router,
               private route: ActivatedRoute)
   {}
-  public ngOnInit(): void {
-    this.exerciseApiCaller.getExercisePage(1).pipe(map((exercises) => {
-      this.exercises = exercises;
-    }), catchError(err => {
-      this.toast.showMessage('No exercise found!', ToastType.INFO);
-      return of(err);
-    })).subscribe(); //TODO pagination
-  }
 
-  private updateVisibility(exercise: Exercise){
-    if (exercise.state == ExerciseState.Active) {
-      this.exerciseApiCaller.setExerciseInactive(exercise.id).pipe(map((success) => {
-        if (success) {
-          exercise.state = ExerciseState.Inactive;
+  private getNode(exercise: Exercise):TreeNode {
+    return {
+      label: exercise.name,
+      children: [
+        {
+          label: 'Statistics',
+          data: exercise.id,
+          type: 'navigateToStatistics'
         }
-      })).subscribe();
-    } else {
-      this.exerciseApiCaller.setExerciseActive(exercise.id).pipe(map((success) => {
-        if (success) {
-          exercise.state = ExerciseState.Active;
-        }
-      })).subscribe();
+      ]
     }
   }
 
-  public onCheckboxPressed(exercise: Exercise): string {
-    this.updateVisibility(exercise)
+  public ngOnInit(): void {
+    this.exerciseApiCaller.getExercisePage(1).subscribe((exercises) => {
+      this.exercises = exercises;
 
-    return `${this.selection.isSelected(exercise.state === ExerciseState.Active) ? 'select' : 'deselect'}`;
-  }
-  public isChecked(exercise: Exercise){
-    return exercise.state === ExerciseState.Active;
-  }
-
-  public onExerciseClicked(row: Exercise) {
-    console.log(row);
+      for (const e of this.exercises) {
+        this.exercisesTree.push(this.getNode(e));
+      }
+    });
   }
 
   public navigateToStatistics(exerciseId: any) {
